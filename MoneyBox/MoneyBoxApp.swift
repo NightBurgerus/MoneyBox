41e42cdbe5ca89c28c3059d053a5dd60cf587f7b
Pasha Terekhov <p.terehov@purpleplane-it.com>
2023-08-14 10:04:51 +0300

    Initial Commit

---
+++ MoneyBox/MoneyBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyBoxApp.swift
+//  MoneyBox
+//
+//  Created by Чебупелина on 14.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct MoneyBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
